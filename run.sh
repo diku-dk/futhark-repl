55a87d0bf3ee672a416f969fdea9f446fd7d1c81
Troels Henriksen <athas@sigkill.dk>
2023-07-14 12:36:23 +0200

    Parameterise on version.

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 DEFAULTPORT=8080
 PORT=$1
+VERSION=0.25.1
 if [ -z "$PORT" ]; then
     PORT=$DEFAULTPORT
 fi
 
 echo "PORT: $PORT"
-sudo docker build -t online-futhark-repl repl
+sudo docker build --build-arg VERSION=${VERSION} -t online-futhark-repl repl
 sudo docker run -p $PORT:$PORT online-futhark-repl --host=0.0.0.0 --port=${PORT} --url-scheme=https --threads=4 views:app
