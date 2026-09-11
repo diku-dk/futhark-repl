019ea9a665c54aa012eaece9110fa1e8b02c839f
Troels Henriksen <athas@sigkill.dk>
2026-09-11 14:09:55 +0200

    Work around stupid Ubuntu thing.

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ fi
 
 echo "PORT: $PORT"
 sudo docker build --build-arg VERSION=${VERSION} -t online-futhark-repl repl
-sudo docker run -p $PORT:$PORT online-futhark-repl --host=0.0.0.0 --port=${PORT} --url-scheme=https --threads=4 views:app
+sudo docker run --security-opt seccomp=unconfined -p $PORT:$PORT online-futhark-repl --host=0.0.0.0 --port=${PORT} --url-scheme=https --threads=4 views:app
